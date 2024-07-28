71dbca2dc3998eefbe0f7f64c19e4fcb13dcac44
DennyCurl <dennycurl@gmail.com>
2024-07-28 15:17:03 +0300

    update nav add buttons: update, delete

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -4,11 +4,8 @@ from django.shortcuts import render
 def index(request):
     data = {
         'title': 'Головна сторінка',
-        'values': ['Some','Hello', 'World'],
+        'values': [],
         'obj': {
-            'car': 'BMW',
-            'age': 18,
-            'hobby': 'Football'
             }
     }
     return render(request, 'main/index.html', data)
