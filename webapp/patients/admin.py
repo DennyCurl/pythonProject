053d4cb1ce3c1d524b467630e3bd868c59b1ef8c
medkh <denny@gmail.com>
2024-09-02 18:02:23 +0300

    update app Patients

--- webapp/patients/admin.py
+++ webapp/patients/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
 
+from .models import Patients
+
+
+class PatientsAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'middle_name', 'birth_date')
+    list_filter = ('brigade', 'date_arrival')
+
+
+admin.site.register(Patients, PatientsAdmin)
+
